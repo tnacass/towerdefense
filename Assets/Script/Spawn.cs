354a8621e6aef7ea2412d5ac92516960ed8ad89a
tom-freetime <tommy.nacass@gmail.com>
2015-05-06 19:00:28 +0800

    Last Version of tommy

--- Assets/Script/Spawn.cs
+++ Assets/Script/Spawn.cs
@@ -6,11 +6,12 @@ public class Spawn : MonoBehaviour
 
 	public GameObject monster;
 	public float spawnRate;
+	public float spawnTime;
 
 	// Use this for initialization
 	void Start () 
 	{
-		InvokeRepeating("SpawnNext", spawnRate, spawnRate);
+		InvokeRepeating("SpawnNext", spawnTime, spawnRate);
 	}
 
 
